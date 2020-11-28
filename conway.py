e78e13ea3320cc241ba00d635aca7c9783e9b7e2
sandeshworld <sb3vf@virginia.edu>
2020-11-28 14:56:07 -0800

    gui works but with bugs

--- conway.py
+++ conway.py
@@ -2,34 +2,20 @@ import numpy as np
 import random
 import time
 import copy
-from game_gui import SimulationGUI
 
 class World:
 
-    def __init__(self,size=10,seed=random.randint(0,10)):
+    def __init__(self,matrix,size=10):
         #for now 2d list but array for larger set in the future
         self.dimension = size
-        self.earth = np.zeros((size,size))
+        self.earth = matrix
         self.iteration = 0
         
-        self.neighbor_count = copy.deepcopy(self.earth) # useful for debugging neighbor count
+        # self.neighbor_count = copy.deepcopy(self.earth) # useful for debugging neighbor count
 
-        #test seeds
-        self.earth[5,5] = 1
-        self.earth[6,5] = 1
-        self.earth[7,5] = 1
-        # self.earth[4,4] = 1
-        # self.earth[4,6] = 1
-        # self.earth[4,3] = 1
-
-        # self.earth[49,50] = 1
-        # self.earth[51,51] = 1
-        # self.earth[52,52] = 1
-        # self.earth[51,53] = 1
-    
 
     #  defines rules for conway's game of life
-    def worst_runtime_rules(self):        
+    def _worst_runtime_rules(self):        
         index_range = range(self.dimension)
         print("Earth")
         print(self.earth)
@@ -39,7 +25,7 @@ class World:
         for row in index_range:
             for column in index_range:
                 num_of_neighbors = self._check_neighbors(row,column)
-                self.neighbor_count[row][column] = num_of_neighbors
+                # self.neighbor_count[row][column] = num_of_neighbors
                 if self.earth[row][column] != 0:
                     if (num_of_neighbors < 2) or (num_of_neighbors > 3):
                         new_world[row][column] = 0
@@ -71,31 +57,10 @@ class World:
 
 
     # got stuck using matplotlib animation... use tkinter instead
-    def start_sim(self,iterations=100):
-        # initializing    
-        for i in range(iterations):
-            self.worst_runtime_rules()
-            # print("Earth")
-            # print(self.earth) # posts earth after modified so doesn't correspond with neighbor count array
-            print("Neighbor Count")
-            print(self.neighbor_count)
-            time.sleep(1)
+    def run_interation(self):
+        self._worst_runtime_rules()
+        return self.earth
+
         
-
-
-
-
-def main():
-    world = World()
-    gui = SimulationGUI() # testing ... it works
-    world.start_sim()
-
-
-
-
-
-if __name__ == "__main__":
-    main()
-
 
 
--- game_gui.py
+++ game_gui.py
@@ -1,34 +1,117 @@
 import Tkinter as tk
-
-
+import numpy as np
+from conway import World
+import time
 
 class SimulationGUI:
 
-    def __init__(self):
+    def __init__(self, squareLength=10):
+        self.size = squareLength
+        # variables
+        self.gui_earth = np.zeros((squareLength,squareLength))
+        
         self.window = tk.Tk()
-        self._setup_frames()
-        self._interactive_grid()    
-        self.startDisplay()  
+        self.window.geometry("1200x900")
+        self._setup_frames(squareLength)
+        
+        self._interactive_grid(squareLength)    
+        self._user_options()
+        
 
     # setup frames to have a display frame and a user buttons frame
-    def _setup_frames(self):
-        self.topFrame = tk.Frame(self.window,height=1000, width=100)
-        self.topFrame.pack(side = "top")
-        self.bottomFrame = tk.Frame(self.window,height=1000, width =1000) # create a frame that will be at the bottom
+    def _setup_frames(self, length):
+        # will keep window size the same no matter the number of buttons but will change button size
+        static_length = 150
+        # pixel frame
+        self.displayFrame = tk.Frame(self.window,height=static_length, width=static_length)
+        self.displayFrame.pack(side="top")
+        # options frame
+        self.bottomFrame = tk.Frame(self.window,height=10, width=static_length) # create a frame that will be at the bottom
         self.bottomFrame.pack(side = "bottom")
 
 
-    def _interactive_grid(self, squareLength = 10):
+
+    def _interactive_grid(self, length):
         #dynamic button creation using lambda function
-        for i in range(squareLength):
-            for j in range(squareLength):
-                b = tk.Button(self.topFrame, bg="white", command=lambda k = (i,j): self.pixelClick(k))
-                b.grid(column=i,row=j)
+        buttonSize = int(20/length)
+        self.pixels = {}
+        for i in range(length):
+            for j in range(length):
+                self.pixels[(i,j)] = tk.Button(self.displayFrame, height=buttonSize, width=buttonSize, bg="white", command=lambda k = (i,j): self.pixelClick(k))
+                self.pixels[(i,j)].grid(row=i,column=j)
 
 
-    def pixelClick(self, (x,y)):
-        print(str(x) + str(y) + "was clicked")
-        
+    def pixelClick(self, (i,j)):
+        self.pixels[(i,j)].configure(bg="red")
+        self.gui_earth[i][j] = 1
+        print self.gui_earth
+
+        #print(str(x) + str(y) + "was clicked")
+
+    def _user_options(self):
+        startSimButton = tk.Button(self.bottomFrame, bg="yellow",height=5, text="START SIM", command=self._startSim)
+        startSimButton.pack(side = "left")
+        resetSimButton = tk.Button(self.bottomFrame, bg="yellow", height=5, text="RESET", command=self._resetSim)
+        resetSimButton.pack(side = "right")
+
+
+    def _startSim(self):
+        print("starting sim")
+        for i in self.pixels:
+            self.pixels[i].configure(state="disabled")
+        # initialize world, where the backend conway sim rules are run
+        self.world = World(self.gui_earth)
+        self._displayLoop()
+    
+    def _displayLoop(self):
+        self.gui_earth = self.world.run_interation()
+        self.updateDisplay()
+        self.window.after(1000,self._displayLoop)
+
+    def _resetSim(self):
+        print("reseting sim")
+        self.gui_earth = np.zeros((self.size,self.size))
+        for i in self.pixels:
+            self.pixels[i].configure(bg="white", state="normal")
+
+
     def startDisplay(self):
         #last method to call
         self.window.mainloop() # maybe this needs to be controlled by main.py?
+
+    def updateDisplay(self):
+        for i in self.pixels:
+            print(i)
+            if self.gui_earth[i[0],i[1]] == 1:
+                self.pixels[i].configure(bg="red")
+            else:
+                self.pixels[i].configure(bg="white")
+            
+
+
+
+def main():
+
+    length = input("Enter size of world (0-30): ")
+    #add upper and lower limit to length size 
+    try:
+        length = 10 if length < 10 else 30 if length > 30 else length
+    except:
+        print("Enter a proper number next time.")
+        print("10 chosen as default.")
+        length = 10
+
+    try:
+        simSpeed = int(input("Enter simulation speed (1 -> slowest, 5 -> fastest"))
+        simSpeed = 1 if simSpeed < 1 else 5 if simSpeed > 5 else simSpeed
+    except:
+        print("Enter a proper number between 1 and 5 next time.")
+        print("1 chosen as default.")
+        simSpeed = 1
+    
+    gui = SimulationGUI(squareLength=length)
+    gui.startDisplay()
+
+
+if __name__ == "__main__":
+    main()
